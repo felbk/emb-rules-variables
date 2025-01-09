772509c6d8e9b33f46c7ab78fd84d2b699dda566
Felbk <lambakfelipe@gmail.com>
2025-01-09 17:42:22 +0000

    tst

--- rules_basic_variable.c
+++ rules_basic_variable.c
@@ -2,14 +2,15 @@
 
 // ..
 
-int a = 0; // var global
-int b = 0; // var global
 
-void foo(void) {
+
+void foo(int *a) {
     a = a + 1; // acessa variavel global
 }
 
 void main(void) {
+    int a = 0; 
+    int b = 0; 
     while (1) {
         foo();
         
